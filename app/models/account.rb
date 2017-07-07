6e76342e82cd3aeae71a89a4033e037f28429e3d
Stéphane Bisinger <stephane.bisinger@renuo.ch>
2017-07-07 11:42:09 +0200

    Add validation on length of first name

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,5 @@
 class Account < ApplicationRecord
-  validates :first_name, presence: { message: 'The first name cannot be empty' }
+  validates :first_name,
+            presence: { message: 'The first name cannot be empty' },
+            length: { maximum: 30, message: 'The first name can be at most %{count} characters long' }
 end
