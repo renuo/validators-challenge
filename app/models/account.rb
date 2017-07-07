3e5119c984574d5957eeedc3030f838be1c0c1b3
Stéphane Bisinger <stephane.bisinger@renuo.ch>
2017-07-07 11:47:19 +0200

    Add validators for last name

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,7 @@ class Account < ApplicationRecord
   validates :first_name,
             presence: { message: 'The first name cannot be empty' },
             length: { maximum: 30, message: 'The first name can be at most %{count} characters long' }
+  validates :last_name,
+            presence: { message: 'The last name cannot be empty' },
+            length: { minimum: 2, message: 'The last name must be at least %{count} characters long' }
 end
